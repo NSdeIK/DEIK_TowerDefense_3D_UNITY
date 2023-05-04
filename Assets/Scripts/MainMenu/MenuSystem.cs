177d96da467fe85724fc32e7b2d79266a8624fe9
NS_deIK <nagysandor199912@gmail.com>
2023-05-04 12:21:14 +0200

    Difficulty UI hozzáadása

---
+++ Assets/Scripts/MainMenu/MenuSystem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSystem : MonoBehaviour
+{
+
+    public GameObject NewGameButton;
+    public GameObject DifficultyPanel;
+
+    public void NewGame()
+    {
+        NewGameButton?.gameObject.SetActive(false);
+        DifficultyPanel?.gameObject.SetActive(true);
+    }
+
+    public void NewGameWithEasyDifficulty()
+    {
+        //TODO - Könnyû scene
+        Debug.Log("Könnyû mód aktiválva");
+    }
+
+    public void NewGameWithHardDifficulty()
+    {
+        //TODO - Nehéz scene
+        Debug.Log("Nehéz mód aktiválva");
+    }
+
+}
